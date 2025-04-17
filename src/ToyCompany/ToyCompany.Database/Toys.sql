a6418333d38d0cd2c94ab0ab74d3cf3322acb938
Lorenzo Baronio <lbaronio@xdatanet.com>
2025-04-17 19:08:50 +0200

    delete database from the project

--- src/ToyCompany/ToyCompany.Database/Toys.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Toys]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Name] NVARCHAR(1000) NOT NULL, 
-    [Description] NVARCHAR(1000) NOT NULL, 
-    [Price] MONEY NOT NULL, 
-    [ImageId] NVARCHAR(30) NOT NULL
-)
