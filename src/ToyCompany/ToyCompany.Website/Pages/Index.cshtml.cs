1c51db7e6d7a00d53f8abed336e0f27f23a507eb
Lorenzo Baronio <lbaronio@xdatanet.com>
2025-04-17 18:16:52 +0200

    create new webapp .NET 9 to replace old webapp dotnetcore3.1

--- src/ToyCompany/ToyCompany.Website/Pages/Index.cshtml.cs
+++ src/ToyCompany/ToyCompany.Website/Pages/Index.cshtml.cs
@@ -1,8 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ToyCompany.Website.Pages
+namespace ToyCompany.Website.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
     {
 
     }
